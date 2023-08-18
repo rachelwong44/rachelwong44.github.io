8451c0ba324b88a220b9667521f3c7a7e9c9e12a
Rachel Wong <rachelwong4444@gmail.com>
2023-08-18 17:22:25 +0100

    Navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,28 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
+
+
+
 export default function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } else{
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
+
   return (
     <>
       <nav className='navbar'>
@@ -31,6 +50,7 @@ export default function Navbar() {
                     </Link>
                 </li>
             </ul>
+            {button && <Button buttonStyle='btn--outline'>Contact Me</Button>}
         </div>
       </nav>
     </>
