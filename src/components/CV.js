daba14b30af1397860dc6e082025f656a9e1b458
Rachel Wong <rachelwong4444@gmail.com>
2023-08-21 16:46:33 +0100

    editing cv

--- src/components/CV.js
+++ src/components/CV.js
@@ -5,11 +5,16 @@ import './CV.css';
 
 function Resume() {
   return (
-    <div className='about-container'>
-      <p>I'm 24 years old and living in London, UK.</p>
-      <p>Data Analyst at a startup trading analytics fintech.</p>
+    <div className='section-container'> <video src='/videos/video-1.mp4' autoPlay loop muted />
+        <div className='about-container'>
+            <h1>Education</h1>
+             <p>University of Oxford - Masters of Chemistry (MCHEM) 4 years</p>
+             <h1>Work</h1>
+             <p>Mosaic Smart Data - Data Analyst (1 year), London</p>
+            <p>Deloitte LLP - Tax Analyst (1 year), London</p>
+         </div>
     </div>
-  );
+);
 }
 
 export default Resume;
