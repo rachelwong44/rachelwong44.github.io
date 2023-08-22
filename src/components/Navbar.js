4617a03394a3a34b9fa6e5e4f49249ff39cfe085
Rachel Wong <rachelwong4444@gmail.com>
2023-08-22 11:51:55 +0100

    small changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,8 +29,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            RACHEL WONG
-            <i class='fa-solid fa-laptop' />
+            <i class='fa-solid fa-laptop' /> 
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
